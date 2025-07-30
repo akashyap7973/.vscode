67ef0f1895bc0d9fe39af637aaa821434cad0206
akashyap <anukashyap7686@gmail.com>
2025-07-29 23:58:16 -0700

    hello

---
+++ first.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a,b;
+    cout<<"enter two numbers:";
+    cin>>a>b;
+    if(a>b) cout<<"grester is:"<<a;
+    if(b>a) cout<<" greater is:"<<b;
+    return 0;
+}
